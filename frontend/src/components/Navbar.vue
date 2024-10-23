e277bbced65411ddef6700d47408ebdd3e71a2bc
MurilloNS <murillo.murillo2001@gmail.com>
2024-10-22 23:44:57 -0300

    Starting Navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-logo">
+      <img src="../assets/images/logo.png" class="logo" alt="Logo" />
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+$navbar-bg: #262523;
+$navbar-color: #fff;
+$hover-color: #f5c67e;
+
+.navbar {
+  display: flex;
+  align-items: center;
+  background-color: $navbar-bg;
+  color: $navbar-color;
+
+  .navbar-logo {
+    .logo {
+      height: 30px;
+      width: auto;
+    }
+  }
+}
+</style>

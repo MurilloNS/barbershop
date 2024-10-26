aae497c0bd01aa49d4b956e7ca41771b0dc4a359
MurilloNS <murillo.murillo2001@gmail.com>
2024-10-25 23:48:29 -0300

    Navbar finished

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,26 +3,96 @@
     <div class="navbar-logo">
       <img src="../assets/images/logo.png" class="logo" alt="Logo" />
     </div>
+
+    <ul class="navbar-links">
+      <li><RouterLink to="/" exact>Home</RouterLink></li>
+      <li><RouterLink to="/servicos">Serviços</RouterLink></li>
+      <li><RouterLink to="/loja">Loja</RouterLink></li>
+      <li><RouterLink to="/sobre">Sobre</RouterLink></li>
+      <li><RouterLink to="/contato">Contato</RouterLink></li>
+    </ul>
+
+    <div class="navbar-auth">
+      <button class="auth-button">Entrar</button>
+      <span class="auth-or">ou</span>
+      <button class="auth-button">Registrar</button>
+    </div>
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <style lang="scss">
-$navbar-bg: #262523;
-$navbar-color: #fff;
-$hover-color: #f5c67e;
+$navbar-bg: #1c1c1c;
+$navbar-color: #f0e68c;
+$hover-color: #d4c65a;
+$button-bg: #f5b748;
+$button-color: #1c1c1c;
+$active-link-color: #ffd700;
 
 .navbar {
   display: flex;
   align-items: center;
   background-color: $navbar-bg;
   color: $navbar-color;
+  height: 70px;
+  padding: 0 8em;
+  gap: 10em;
 
   .navbar-logo {
     .logo {
-      height: 30px;
+      height: 80px;
       width: auto;
+      padding-top: 0.5em;
+    }
+  }
+
+  .navbar-links {
+    list-style: none;
+    display: flex;
+    gap: 4em;
+    margin: 0;
+    padding: 0;
+
+    li {
+      a {
+        color: $navbar-color;
+        text-decoration: none;
+        font-size: 1.2rem;
+        transition: color 0.3s ease;
+
+        &:hover {
+          color: $hover-color;
+        }
+      }
+
+      .router-link-active {
+        color: $active-link-color;
+        font-weight: bold;
+      }
+    }
+  }
+}
+
+.navbar-auth {
+  display: flex;
+  align-items: center;
+  gap: 1em;
+
+  .auth-button {
+    background-color: $button-bg;
+    color: $button-color;
+    border: none;
+    padding: 0.3em 0.8em;
+    font-size: 1rem;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    border-radius: 5%;
+
+    &:hover {
+      background-color: adjust-color($button-bg, $lightness: -10%);
     }
   }
 }
